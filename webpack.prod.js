019db6d196642ba96f0f9871d28b151cef280e0b
Anmizi <1845513904@qq.com>
2022-07-03 18:00:30 +0800

    chore : modifiy webpack config files

---
+++ webpack.prod.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.join(__dirname, './src', 'index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[chunkhash:8].js'
+  },
+  module: {
+    //指定加载规则
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(less|css)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  // 定义模块的解析方式
+  resolve: {
+    extensions: ['.ts', '...']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './public', 'index.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:8].css'
+    })
+  ]
+
+}
